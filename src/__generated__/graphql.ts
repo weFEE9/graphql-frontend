6386337eaa5583a980ff63475eaa92edfa9f6065
weeee9 <teletubby332@gmail.com>
2024-02-04 11:08:20 +0800

    chore: gen graphql functionality

---
+++ src/__generated__/graphql.ts
@@ -0,0 +1,64 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type Author = {
+  __typename?: 'Author';
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+};
+
+export type Book = {
+  __typename?: 'Book';
+  author: Author;
+  id: Scalars['ID']['output'];
+  title: Scalars['String']['output'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  book?: Maybe<Book>;
+  books: Array<Book>;
+};
+
+
+export type QueryBookArgs = {
+  input?: InputMaybe<QueryBookInput>;
+};
+
+
+export type QueryBooksArgs = {
+  input?: InputMaybe<QueryBooksInput>;
+};
+
+export type QueryBookInput = {
+  id: Scalars['ID']['input'];
+};
+
+export type QueryBooksInput = {
+  title?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type QueryBooksQueryVariables = Exact<{
+  title?: InputMaybe<Scalars['String']['input']>;
+}>;
+
+
+export type QueryBooksQuery = { __typename?: 'Query', books: Array<{ __typename: 'Book', id: string, title: string, author: { __typename: 'Author', id: string, name: string } }> };
+
+
+export const QueryBooksDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"queryBooks"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"title"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"books"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"title"},"value":{"kind":"Variable","name":{"kind":"Name","value":"title"}}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"author"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}}]}}]} as unknown as DocumentNode<QueryBooksQuery, QueryBooksQueryVariables>;
