8b74113e71216f9c2ee214bf3d6a9d2c26c2a8cc
weeee9 <teletubby332@gmail.com>
2024-02-04 11:08:31 +0800

    feat: refactor file imports and structure in App and index files
    - Delete `src/App.css` file
    - Modify `src/App.tsx` file:
      - Import `useState` from `react`
      - Import `useLazyQuery` from `@apollo/client`
      - Import `graphql` from `./__generated__/gql`
      - Add `searchInput` state variable and `handleSearchInputChange` function
      - Add `getBooks` and `data, loading, error` variables using `useLazyQuery`
      - Add loading and error handling conditions
      - Modify JSX structure
    - Modify `src/index.tsx` file:
      - Import `ApolloClient, InMemoryCache, ApolloProvider` from `@apollo/client`
      - Create `client` instance of `ApolloClient`
      - Wrap `App` component with `ApolloProvider` component
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import { useLazyQuery } from '@apollo/client';
+import { graphql } from './__generated__/gql';
+
+const queryBooks = graphql(/* GraphQL */ `
+  query queryBooks($title: String) {
+    books(input: { title: $title }) {
+      id
+      title
+      author {
+        id
+        name
+        __typename
+      }
+      __typename
+    }
+  }
+`);
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  const [getBooks, { data, loading, error }] = useLazyQuery(queryBooks);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Welcome to Bookstore</h1>
+      <input
+        type='text'
+        placeholder='Search books'
+        onChange={handleSearchInputChange}
+        value={searchInput}
+      />
+      <button onClick={() => getBooks({ variables: { title: searchInput } })}>
+        Search
+      </button>
+      {data?.books.length ? (
+        <ul>
+          {data.books.map((book) => (
+            <li key={book.id}>
+              {book.title} - {book.author.name}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No results</p>
+      )}
     </div>
   );
 }
